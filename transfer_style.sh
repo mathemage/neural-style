773fd5fb4facaefc2bfbfc5ea10f45e2dc4c773e
Karel Ha <mathemage@gmail.com>
2017-06-30 23:57:16 +0200

    Add invoices

--- transfer_style.sh
+++ transfer_style.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-DEFAULT_FILEDIR="/home/mathemage/Downloads/a-star-team/png/"
-CONTENT_FILE=${1:-$DEFAULT_FILEDIR/03ab699c89aeae4bae28f06befc95a11-0.png}
-STYLE_FILE=${2:-$DEFAULT_FILEDIR/02bf6d075d44b03b0650ffbaa98e2ea0-0.png}
+#DEFAULT_FILEDIR="/home/mathemage/Downloads/a-star-team/png/"
+DEFAULT_FILEDIR="./invoices"
+CONTENT_FILE=${1:-$DEFAULT_FILEDIR/content.png}
+STYLE_FILE=${2:-$DEFAULT_FILEDIR/style.png}
 OUTPUT_FILE=${3:-$DEFAULT_FILEDIR/invoice-transferred.png}
 ITERATIONS=5
 
